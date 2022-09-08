1a06d603013f6a28a31a8e9d4c43b0116f8ae123
Chris Domingos <uhy9xj@virginia.edu>
2022-09-08 20:40:46 +0000

    hw start

---
+++ src/mywork/hw2.py
@@ -0,0 +1,20 @@
+from z3 import *
+def hw2():
+    s = Solver()
+    X, Y, Z = Bools('X Y Z')
+    
+    #X ∨ Y, X ⊢ ¬Y
+    #((X \or Y) \and X) \implies \notY
+    C1 = Implies(And((Or(X, Y), X)), Not(Y))
+    
+    
+    
+    s.add(Not(C1))
+    #I believe it's not valid
+    
+    r = s.check(C1)
+    
+    if(r == unsat):
+        print("C1 is valid")
+    else:
+        print("Here's a counter example ", s.model())
