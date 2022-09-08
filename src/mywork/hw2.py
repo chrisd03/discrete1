f1d3370516656486925ce11087fe2f32c9a9849a
Chris Domingos <uhy9xj@virginia.edu>
2022-09-08 20:44:42 +0000

    kms

--- src/mywork/hw2.py
+++ src/mywork/hw2.py
@@ -1,18 +1,19 @@
 from z3 import *
 def hw2():
-    s = Solver()
+    
     X, Y, Z = Bools('X Y Z')
+    s = Solver()
     
     #X ∨ Y, X ⊢ ¬Y
     #((X \or Y) \and X) \implies \notY
-    C1 = Implies(And((Or(X, Y), X)), Not(Y))
+    C1 = Implies(And(Or(X, Y), X), Not(Y))
     
     
     
     s.add(Not(C1))
     #I believe it's not valid
     
-    r = s.check(C1)
+    r = s.check()
     
     if(r == unsat):
         print("C1 is valid")
