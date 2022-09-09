81563cbcf1ee75c58f455e4cebf6e3fb79fd5b2e
Chris Domingos <uhy9xj@virginia.edu>
2022-09-09 16:48:25 +0000

    lets gooooo

--- src/mywork/hw2.py
+++ src/mywork/hw2.py
@@ -152,4 +152,126 @@ def hw2():
         print("C11 is valid")
     else:
         print("Here's a counter example", s.model()) 
+        
+    #X ↔ Y ⊢ Y → X
+    #(X biimplies Y) implies (Y implies X)
+    #If X is equivalent to Y then if Y is true X is true
+    C12 = Implies(X == Y, Implies(Y, X))
+    s.reset()
+    s.add(Not(C12))
+    #I believe it's valid
+    r = s.check()
+    if(r == unsat):
+        print("C12 is valid")
+    else:
+        print("Here's a counter example", s.model()) 
+        
+    
+    #X ∨ Y, X → Z, Y → Z ⊢ Z
+    #((X or Y) and (X implies Z) and (Y implies Z)) implies Z
+    #If X is true or Y is true, and Z is true if X is true, and Z is true if Y is true then Z is true
+    C13 = Implies(And(Or(X, Y), Implies(X, Z), Implies(Y, Z)), Z)
+    s.reset()
+    s.add(Not(C13))
+    #I believe it's valid
+    r = s.check()
+    if(r == unsat):
+        print("C13 is valid")
+    else:
+        print("Here's a counter example", s.model()) 
+        
+    
+    #X → Y, Y ⊢ X  
+    #((X implies Y) and Y) implies X 
+    #If Y is true when X is true, and Y is true then X must be true
+    C14 = Implies(And(Implies(X, Y), Y), X)
+    s.reset()
+    s.add(Not(C14))
+    #I believe it's not valid
+    r = s.check()
+    if(r == unsat):
+        print("C14 is valid")
+    else:
+        print("Here's a counter example", s.model()) 
+        
+    
+    #X → Y, X ⊢ Y
+    #((X implies Y) and X) implies Y 
+    #If Y is true when X is true, and X is true then X must be true
+    C15 = Implies(And(Implies(X, Y), X), Y)
+    s.reset()
+    s.add(Not(C15))
+    #I believe it's valid
+    r = s.check()
+    if(r == unsat):
+        print("C15 is valid")
+    else:
+        print("Here's a counter example", s.model())     
+    
+    
+    #X → Y, Y → Z ⊢ X → Z
+    #((X implies Y) and (Y implies Z)) implies (X implies Z)
+    #If Y is true when X is true, and Z is true when Y is true then Z must be true when X is true
+    C16 = Implies(And(Implies(X, Y), Implies(Y, Z)), Implies(X, Z))
+    s.reset()
+    s.add(Not(C16))
+    #I believe it's valid
+    r = s.check()
+    if(r == unsat):
+        print("C16 is valid")
+    else:
+        print("Here's a counter example", s.model()) 
+        
+    #X → Y ⊢ Y → X
+    #(X implies Y) implies (Y implies X)
+    #If Y is true when X is true then X is true when Y is true
+    C17 = Implies(Implies(X, Y), Implies(Y, X))
+    s.reset()
+    s.add(Not(C17))
+    #I believe it's not valid
+    r = s.check()
+    if(r == unsat):
+        print("C17 is valid")
+    else:
+        print("Here's a counter example", s.model()) 
+        
+    #X → Y ⊢ ¬Y → ¬X
+    #(X implies Y) implies (Not(Y) implies (Not(X))
+    #If Y is true when X is true then when Y is false X is false
+    C18 = Implies(Implies(X, Y),Implies(Not(Y), Not(X)))
+    s.reset()
+    s.add(Not(C18))
+    #I believe it's valid
+    r = s.check()
+    if(r == unsat):
+        print("C18 is valid")
+    else:
+        print("Here's a counter example", s.model()) 
+        
+    #¬(X ∨ Y) ↔ ¬X ∧ ¬Y
+    #Not(X or Y) biimplies Not(X) and Not(Y)
+    #X or Y being false is equivalent to X and Y both being false
+    C19 = Not(Or(X, Y)) == And(Not(X), Not(Y))
+    s.reset()
+    s.add(Not(C19))
+    #I believe it's valid
+    r = s.check()
+    if(r == unsat):
+        print("C19 is valid")
+    else:
+        print("Here's a counter example", s.model()) 
+        
+    
+    #¬(X ∧ Y) ↔ ¬X ∨ ¬Y
+    #Not(X and Y) biimplies Not(X) or Not(Y)
+    #X and Y being false is equivalent to X being false or Y being false
+    C20 = Not(And(X, Y)) == Or(Not(X), Not(Y))
+    s.reset()
+    s.add(Not(C20))
+    #I believe it's valid
+    r = s.check()
+    if(r == unsat):
+        print("C20 is valid")
+    else:
+        print("Here's a counter example", s.model())
 hw2()
